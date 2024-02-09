8fdbd9ff587501ec8e71e32ce08b3eee1a738833
Oshadhi2 <90508862+Oshadhi2@users.noreply.github.com>
2024-02-09 04:39:17 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Services from './Component/Services';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Services/>
+      
+      
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ Services.js
@@ -0,0 +1,167 @@
+import React from 'react';
+
+
+export default function Services() {
+  return (
+    <section className='section-white'>
+         <div className='container'>
+            <div className='row'>
+                <div className='topic'>
+                    <h2 className='company'>VERITECH SOFTWARE IT SERVICE</h2>
+                    
+                    <p className='section-subtitle'>Welcome to Veritech's dedicated Software Development Team! At Veritech, innovation and excellence converge as our talented professionals collaborate to design, build, and deploy cutting-edge software solutions. Meet the individuals who bring creativity, expertise, and passion to the heart of our technological advancements.</p>
+                    
+                    <p>----------------------------------------------------------------------------------------------------------------</p>
+                    <h2 className='section-title'>Our Team Members</h2>
+                </div>
+
+                        
+            </div>
+        </div> 
+
+      
+        <h2 className='team-name'>Management Team</h2>
+        
+            
+        <div class="box-container">
+            
+            <div class="box">
+                <img src="https://png.pngtree.com/png-vector/20231015/ourmid/pngtree-man-looking-far-away-wearing-a-gray-office-vest-png-image_10162910.png" alt=""/>
+                <div class="content">
+                    <h3>JOHN MECHEL</h3>
+                    <h4>Veritech Manager</h4>
+                    <p>John is a seasoned IT professional with a wealth of experience in service management. As the VERITECH manager, he oversees the entire service delivery process, ensuring alignment with business objectives. </p>
+                    
+                </div>
+            </div>
+            <div class="box">
+                <img src="https://img.freepik.com/free-photo/portrait-happy-woman_186202-621.jpg" alt=""/>
+                <div class="content">
+                    <h3>JENY THOMAS</h3>
+                    <h4>Service Desk Analyst</h4>
+                    <p>Jeny is the first point of contact for end-users seeking assistance. With excellent communication skills, she troubleshoots and resolves issues efficiently. </p>
+                    
+                </div>
+            </div>
+            <div class="box">
+                <img src="https://pngimg.com/uploads/man/man_PNG6531.png" alt=""/>
+                <div class="content">
+                    <h3>ROJAR PRAKASH</h3>
+                    <h4>Change Management Specialist </h4>
+                    <p>Rojar plays a crucial role in assessing and implementing changes to IT services. He evaluates potential risks, coordinates with stakeholders, and ensures changes are executed seamlessly.</p>
+                    
+                </div>
+            </div>
+            <div class="box">
+                <img src="	https://clipart-library.com/image_gallery2/Man-PNG-File.png" alt=""/>
+                <div class="content">
+                    <h3>DENIS DAVIS</h3>
+                    <h4>Configuration Management Analyst</h4>
+                    <p>Denis is responsible for maintaining the configuration management database. He ensures accurate records of IT assets, their relationships, and configurations. </p>
+                    
+                </div>
+            </div>
+            
+            
+        </div> 
+
+
+
+
+        <h2 className='team-name'>Development Team</h2>
+        
+            
+        <div class="box-container">
+            
+            <div class="box">
+                <img src="	https://freepngimg.com/save/22654-man/594x600" alt=""/>
+                <div class="content">
+                    <h3>HARRISON SMITH </h3>
+                    <h4>Software Development Manager </h4>
+                    <p>Harrison leads the development team, overseeing project timelines, resource allocation, and ensuring adherence to coding standards. He works closely with stakeholders to understand project requirements and manages the team's overall performance.</p>
+                    
+                </div>
+            </div>
+            <div class="box">
+                <img src="https://img.freepik.com/free-photo/young-woman-isolated-white-wall_231208-11508.jpg " alt=""/>
+                <div class="content">
+                    <h3>DANI WILLIAMS</h3>
+                    <h4>Project Manager</h4>
+                    <p>Dani focuses on planning, execution, and delivery of software projects. She manages timelines, budgets, and resources, ensuring that the development team meets project goals efficiently.</p>
+                    
+                </div>
+            </div>
+            <div class="box">
+                <img src="	https://www.pngall.com/wp-content/uploads/5/Cool-Model-Man.png" alt=""/>
+                <div class="content">
+                    <h3>BENJAMIN CARTER</h3>
+                    <h4>Lead Developer/Technical Lead</h4>
+                    <p>Benjamin is responsible for guiding the technical aspects of the development process. He leads the development team in making architectural decisions, ensures code quality, and mentors junior developers.</p>
+                    
+                </div>
+            </div>
+            
+            
+        </div>   
+
+
+
+        <h2 className='team-name'>Marketing Team</h2>
+        
+            
+        <div class="box-container">
+
+            <div class="box">
+                <img src="	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZb65W6CLwJSVc6OsbMSwEh1sUrCs_SGsBFQ&usqp=CAU" alt=""/>
+                <div class="content">
+                    <h3>EMMA TUNER</h3>
+                    <h4>Chief Marketing Officer</h4>
+                    <p>Emma oversees the entire marketing strategy, planning, and execution. She works closely with other departments to align marketing initiatives with overall business goals and ensures the team is driving effective campaigns.</p>
+                    
+                </div>
+            </div>
+            
+            <div class="box">
+                <img src="	https://www.transparentpng.com/thumb/man/psW26l-download-man-png-image-png-image-freepngimg.png" alt=""/>
+                <div class="content">
+                    <h3>ALEX RODRIGUIZ </h3>
+                    <h4>Marketing Manager</h4>
+                    <p>Alex is responsible for developing and executing marketing campaigns. He manages the marketing team, coordinates efforts, and ensures that strategies are implemented effectively to reach target audiences.</p>
+                    
+                </div>
+            </div>
+            
+            <div class="box">
+                <img src="https://www.pngall.com/wp-content/uploads/2018/04/Businessman-Transparent.png" alt=""/>
+                <div class="content">
+                    <h3>NATHAN PATEL</h3>
+                    <h4>Digital Marketing Specialist</h4>
+                    <p>Nathan specializes in online marketing channels, including social media, email marketing, and paid advertising. He monitors digital trends, optimizes campaigns, and uses analytics to measure and improve the effectiveness of digital marketing efforts.</p>
+                    
+                </div>
+            </div>
+            
+            
+        </div>   
+
+        <div className='footer'>
+            <footer>
+                <p>Copyright &copy; 2024 by Refsnes Data. All rights reserved.... </p>
+            </footer>
+        </div>
+
+
+
+      
+                              
+
+    
+
+    </section>
+       
+    )  
+
+ }
+
+    
+  
